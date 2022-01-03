2b0170372f6d300e45ac5968859cc2d3b6a8fc93
Narutoboy <akshaysharma21111995@gmail.com>
2022-01-03 15:02:16 +0530

    Initial How to use row in Room DB

--- app/src/main/java/com/do_big/diginotes/activity/SplashScreen.java
+++ app/src/main/java/com/do_big/diginotes/activity/SplashScreen.java
@@ -1,9 +1,6 @@
 package com.do_big.diginotes.activity;
 
-import static com.do_big.diginotes.utils.AppConstant.DATABASE_NAME;
-
 import android.content.Intent;
-import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
@@ -12,12 +9,8 @@ import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.do_big.diginotes.R;
-import com.do_big.diginotes.model.Note;
-import com.do_big.diginotes.model.NoteViewModel;
 import com.do_big.diginotes.utils.PrefManager;
 
-import java.util.Calendar;
-import java.util.Date;
 import java.util.Timer;
 import java.util.TimerTask;
 
@@ -40,18 +33,17 @@ public class SplashScreen extends AppCompatActivity {
             TimerTask tt = new TimerTask() {
                 @Override
                 public void run() {
-                    insert();
+
                     startActivity(new Intent(SplashScreen.this, WelcomeActivity.class));
                     SplashScreen.this.finish();
 
                 }
             };
-            Timer t=new Timer();
+            Timer t = new Timer();
             t.schedule(tt, WELCOME_DELAY);
 
-        }
-        else{
-            TimerTask tt=new TimerTask() {
+        } else {
+            TimerTask tt = new TimerTask() {
                 @Override
                 public void run() {
                     Intent intent = new Intent(SplashScreen.this, MainActivity.class);
@@ -59,29 +51,13 @@ public class SplashScreen extends AppCompatActivity {
                     SplashScreen.this.finish();
                 }
             };
-            Timer t=new Timer();
+            Timer t = new Timer();
             t.schedule(tt, SPLASH_DELAY);
 
 
         }
 
 
-
-    }
-
-    private void insert() {
-        //insert How to use in room DB
-
-        String etNote = "Some of Digi Note feature :-\n" +
-                "1. Add note either via share or write or Voice .\n" +
-                "2. Search notes (Title or Date ) . \n" +
-                "3. Having Setting so it will easy to focus on learning .\n" +
-                "4. Edit TextSize and Night Mode .\n" +
-                "5. Stay with us lot of New feature and on the way .\n" +
-                "Happy Learning :)";
-        String title = "How to Use ";
-        Date createdAt = Calendar.getInstance().getTime();
-        NoteViewModel.insert(new Note(etNote, title, createdAt, true, null));
     }
 
 
