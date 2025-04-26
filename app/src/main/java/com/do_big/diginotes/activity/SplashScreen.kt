4cf7bffa4c013b230efcdfdce462259bcc3752c1
akshaysharma <erakshaysharma21@gmail.com>
2025-04-26 17:05:32 +0530

    Kotlin | Migrated SplashScreen to kt

--- app/src/main/java/com/do_big/diginotes/activity/SplashScreen.kt
+++ app/src/main/java/com/do_big/diginotes/activity/SplashScreen.kt
@@ -1,57 +1,38 @@
-package com.do_big.diginotes.activity;
+package com.do_big.diginotes.activity
 
-import android.content.Intent;
-import android.os.Bundle;
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import androidx.appcompat.app.AppCompatActivity
+import com.do_big.diginotes.databinding.ActivitySplashScreenBinding
+import com.do_big.diginotes.utils.PrefManager
 
-import androidx.appcompat.app.AppCompatActivity;
+class SplashScreen : AppCompatActivity() {
 
-import com.do_big.diginotes.R;
-import com.do_big.diginotes.utils.PrefManager;
-
-import java.util.Timer;
-import java.util.TimerTask;
-
-public class SplashScreen extends AppCompatActivity {
-
-
-    public static final int SPLASH_DELAY = 2500;
-    public static final int WELCOME_DELAY = 1000;
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_splash_screen);
-        PrefManager prefManager = new PrefManager(this);
-        if (prefManager.isFirstTimeLaunch()) {
-            TimerTask tt = new TimerTask() {
-                @Override
-                public void run() {
-
-                    startActivity(new Intent(SplashScreen.this, WelcomeActivity.class));
-                    SplashScreen.this.finish();
-
-                }
-            };
-            Timer t = new Timer();
-            t.schedule(tt, WELCOME_DELAY);
-
-        } else {
-            TimerTask tt = new TimerTask() {
-                @Override
-                public void run() {
-                    Intent intent = new Intent(SplashScreen.this, HomeActivity.class);
-                    startActivity(intent);
-                    SplashScreen.this.finish();
-                }
-            };
-            Timer t = new Timer();
-            t.schedule(tt, SPLASH_DELAY);
-
-
-        }
-
-
+    companion object {
+        const val SPLASH_DELAY = 2500L // Use Long for milliseconds
+        const val WELCOME_DELAY = 1000L
     }
 
+    private lateinit var binding: ActivitySplashScreenBinding
 
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivitySplashScreenBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val prefManager = PrefManager(this)
+        val isFirstTimeLaunch = prefManager.isFirstTimeLaunch
+
+        val delay = if (isFirstTimeLaunch) WELCOME_DELAY else SPLASH_DELAY
+        val targetActivity =
+            if (isFirstTimeLaunch) WelcomeActivity::class.java else HomeActivity::class.java
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            val intent = Intent(this, targetActivity)
+            startActivity(intent)
+            finish()
+        }, delay)
+    }
 }
